2e74afd087843910ed0edbecc2dad436607dc2b3
VanoKi <kirsvano@gmail.com>
2025-02-26 19:55:47 +0300

    feat: move accordion to another file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { log } from 'console';
+import Accordion from './components/accordion/accordion';
 
 // func decloration
 function App() { // this is a component
@@ -54,30 +55,6 @@ function Rating() {
       <Star/>
     </>
   )
-}
-
-function Accordion() {
-  console.log('accordion rendered')
-  // debugger
-  return <div>
-    <AccordionTitle/>
-    <AccordionBody/>
-  </div>
-}
-
-function AccordionTitle() {
-  console.log('AccordionTitle rendered')
-  return <h3>Menu</h3>
-}
-function AccordionBody() {
-  console.log('AccordionBody rendered')
-  return <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-  </ul>
 }
 
 function Star() {

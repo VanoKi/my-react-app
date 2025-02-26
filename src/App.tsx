8f2e9949856f29c626357060dde55d1414c6cdd8
VanoKi <kirsvano@gmail.com>
2025-02-26 18:30:59 +0300

    feat: add hello and debugger

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function hello() {
+  debugger
+  alert('Hello')
+}
+// hello()
+
 // func decloration
-function App() {
+function App() { // this is a component
+  // something useful
+  // return jsx
   return (
     <div>
       This is app component
@@ -28,5 +36,28 @@ function App() {
 //     </div>
 //   );
 // }
+
+function Rating() {
+  return (
+    <div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+    </div>
+  )
+}
+
+function Accordion() {
+  return <div>
+    <h3>Menu</h3>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
+}
 
 export default App;

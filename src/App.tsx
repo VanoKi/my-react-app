3e92eb9478ec89447b0fc2ed27d4f410bf6ea6f4
VanoKi <kirsvano@gmail.com>
2025-02-26 17:58:22 +0300

    feat: add three types of functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// func decloration
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      This is app component
     </div>
   );
 }
+
+// expression
+// const App = function() {
+//   return (
+//     <div>
+//       This is app component
+//     </div>
+//   );
+// }
+
+// arrow
+// const App = () => {
+//   return (
+//     <div>
+//       This is arrow component
+//     </div>
+//   );
+// }
 
 export default App;
